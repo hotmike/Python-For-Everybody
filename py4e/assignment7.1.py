fca490114305b6f84b4d10c761cc14f6bc8bbbe6
hotmike <75709975+hotmike@users.noreply.github.com>
2022-08-25 11:43:15 -0400

    Update assignment7.1.py

--- py4e/assignment7.1.py
+++ py4e/assignment7.1.py
@@ -1,7 +1,8 @@
-# Learn how to manage file
+Learn how to manage file
 
 fname = input("Enter file name: ")
 fh = open(fname)
 for x in fh:
     x = x.rstrip() #取消右邊空白
     print(x.upper()) #把每一行變大寫列出來
+    
